a57df7a280ac537c7fb71495bcf5424bbe37c35c
Conor Taylor <taylorconor@me.com>
2015-12-09 07:37:11 +0000

    Implementing node commands

--- node.py
+++ node.py
@@ -1,3 +1,4 @@
+import re, os, errno
 from server import Server
 
 class Node(object):
@@ -6,10 +7,48 @@ class Node(object):
     def request_handler(self, conn):
         try:
             data = conn.recv(8096)
-            print "request_handler: request="+data
-            conn.send("Thanks!")
+            input = data.split()
+            if input[0] == "GET":
+                filename = self._dir + input[1]
+                if not os.path.isfile(filename):
+                    conn.send("NO_EXIST")
+                else:
+                    conn.send("Handling GET request for file: " + input[1])
+
+            elif input[0] == "PUT":
+                filepath = os.path.dirname(input[1])
+                if not os.path.isdir(self._dir + filepath):
+                    conn.send("NO_EXIST")
+                else:
+                    conn.send("Handling PUT request for file: " + input[1])
+
+            elif input[0] == "MKDIR":
+                newdir = self._dir + input[1]
+                # remove trailing slash, if any
+                while newdir.endswith('/'):
+                    newdir = newdir[:len(newdir)-1]
+                basedir = os.path.dirname(newdir)
+                if not os.path.isdir(basedir):
+                    conn.send("NO_EXIST")
+                else:
+                    try:
+	                    os.makedirs(newdir)
+                    except Exception as e:
+	                    if e.errno != errno.EEXIST:
+		                    raise
+
+            elif input[0] == "DELETE":
+                filename = self._dir + input[1]
+                if not os.path.isfile(filename):
+                    conn.send("NO_EXIST")
+                else:
+                    conn.send("Handling DELETE request for file: " + input[1])
+
+            else:
+                conn.send("ERR")
             conn.close()
-        except e:
+        except Exception as e:
+            conn.send("ERR")
             conn.close()
 
     def __init__(self, dir):
